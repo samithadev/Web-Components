fd87abbe83256c823471c98936425440113dde6c
Samitha Wickramasinghe <89331663+samithadev@users.noreply.github.com>
2024-02-05 00:50:35 +0530

    feat:initiate home and imageslider pages: add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home/home.page.jsx";
+import MainPage from "./pages/main/main.page.jsx";
+import ImageSlider from "./pages/imageSlider/slider.page.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/card-scroll",
+    element: <MainPage />,
+  },
+  {
+    path: "/image-slider",
+    element: <ImageSlider />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
