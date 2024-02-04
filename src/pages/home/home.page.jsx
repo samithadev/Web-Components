87620500d5f42c5a0af9aca8db256e8e34118ba4
Samitha Wickramasinghe <89331663+samithadev@users.noreply.github.com>
2024-02-05 01:16:55 +0530

    feat:create home page with links

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
-    <div>
-      <h1>This is Home Page</h1>
+    <div className="flex flex-col items-center justify-center h-[100vh]">
+      <h1 className=" text-3xl font-semibold">Animations</h1>
+
+      <ul className=" text-xl mt-8 text-center text-white">
+        <Link to={"/card-scroll"}>
+          <li className=" bg-black p-3">Cards</li>
+        </Link>
+        <Link>
+          <li className=" bg-slate-600 p-3 mt-2">Image Slider</li>
+        </Link>
+      </ul>
     </div>
   );
 }
--- src/pages/main/main.page.jsx
+++ src/pages/main/main.page.jsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import Card from "./components/Card";
 import { useScroll } from "framer-motion";
 import { projects } from "../../data";
+import { Link } from "react-router-dom";
 
 export default function MainPage() {
   const container = useRef(null);
@@ -12,23 +13,32 @@ export default function MainPage() {
   });
 
   return (
-    <div
-      ref={container}
-      className=" flex flex-col items-center justify-center mb-[100vh]"
-    >
-      {projects.map((project, i) => {
-        const targetScale = 1 - (projects.length - i) * 0.05;
-        return (
-          <Card
-            key={i}
-            i={i}
-            {...project}
-            progress={scrollYProgress}
-            range={[i * 0.25, 1]}
-            targetScale={targetScale}
-          />
-        );
-      })}
+    <div>
+      <div className=" fixed top-2 left-2">
+        <Link to={"/"}>
+          <h1 className=" text-lg bg-black text-white p-3 rounded-full">
+            Home
+          </h1>
+        </Link>
+      </div>
+      <div
+        ref={container}
+        className=" flex flex-col items-center justify-center mb-[100vh]"
+      >
+        {projects.map((project, i) => {
+          const targetScale = 1 - (projects.length - i) * 0.05;
+          return (
+            <Card
+              key={i}
+              i={i}
+              {...project}
+              progress={scrollYProgress}
+              range={[i * 0.25, 1]}
+              targetScale={targetScale}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
